7cba5eacabe7982049b6168ae9688aaaaa4788ba
hyeyoung0112 <hyeyoung0112@gmail.com>
2018-12-27 21:13:36 +0900

    wow contacts appear in text now

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,44 +1,52 @@
 package com.example.myapplication;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListView;
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ListView listView;
-    private CustomAdapter customAdapter;
-    private ArrayList<ContactModel> contactModelArrayList;
+    private Button loadContacts;
+    private TextView listContacts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listContacts = (TextView) findViewById(R.id.listContacts);
+        loadContacts();
+    }
 
-        listView = findViewById(R.id.listView);
+    private void loadContacts(){
+        StringBuilder builder = new StringBuilder();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
-        contactModelArrayList = new ArrayList<>();
+        if(cursor.getCount() > 0) {
+            while (cursor.moveToNext()){
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
 
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
-        while (phones.moveToNext())
-        {
-            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-            ContactModel contactModel = new ContactModel();
-            contactModel.setName(name);
-            contactModel.setNumber(phoneNumber);
-            contactModelArrayList.add(contactModel);
-            Log.d("name>>",name+"  "+phoneNumber);
+                if (hasPhoneNumber > 0){
+                    Cursor cursor2 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[] {id}, null);
+                    while (cursor2.moveToNext()){
+                        String phoneNumber = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        builder.append("Contact: ").append(name).append(", Phone Number : ").append(phoneNumber).append("\n\n");
+                    }
+                    cursor2.close();
+                }
+            }
         }
-        phones.close();
+        cursor.close();
 
-        customAdapter = new CustomAdapter(this,contactModelArrayList);
-        listView.setAdapter(customAdapter);
-
+        listContacts.setText(builder.toString());
     }
 }
