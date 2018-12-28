a109fa8ed40d1dcffd8e5e5cb933b0706ff99ace
hyeyoung0112 <hyeyoung0112@gmail.com>
2018-12-28 13:44:50 +0900

    repush

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
