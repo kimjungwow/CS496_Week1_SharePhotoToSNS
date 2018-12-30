523b3f33be3aac0cea5ac56135331d00026ca4d1
hyeyoung0112 <hyeyoung0112@gmail.com>
2018-12-30 15:41:51 +0900

    tab3 layout and java tab file updated

--- app/src/main/java/com/example/myapplication/Tab3Etc.java
+++ app/src/main/java/com/example/myapplication/Tab3Etc.java
@@ -1,17 +1,97 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Tab3Etc extends Fragment {
+    private View cameraButton;
+    private View galleryButton;
+    private View shareButton;
+    private View saveButton;
+    private ImageView newPicture;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3etc, container, false);
+        cameraButton = rootView.findViewById(R.id.cameraButton);
+        galleryButton = rootView.findViewById(R.id.galleryButton);
+        shareButton = rootView.findViewById(R.id.shareButton);
+        saveButton = rootView.findViewById(R.id.saveButton);
+        newPicture = rootView.findViewById(R.id.newImage);
+
+        //open camera app
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        //open imagepicker
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+
+        });
+
+        //share main image
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+
+        });
+
+        //save image to gallery
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (WritePermissionCheck()){
+                    BitmapDrawable filteredDrawable = (BitmapDrawable) newPicture.getDrawable();
+                    Bitmap newBP = filteredDrawable.getBitmap();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                    String title = sdf.format(new Date());
+                    MediaStore.Images.Media.insertImage(getContext().getContentResolver(), newBP ,title, "description");
+                }
+
+                else {
+                    Toast.makeText(getContext(), "Cannot save image.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         return rootView;
+    }
+
+    public int checkselfpermission(String permission) {
+        return PermissionChecker.checkSelfPermission(getContext(), permission);
+    }
+
+    public boolean WritePermissionCheck() {
+        if (checkselfpermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+            if (checkselfpermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
